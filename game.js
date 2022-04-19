d7a75891b4e9ff77737bf4c2057adf16e6db33d6
Sami Barchid <sami.barchid@univ-lille.fr>
2022-04-19 15:08:37 +0200

    game

--- game.js
+++ game.js
@@ -9,105 +9,79 @@ canSecondJump = false
 
 
 // initialize context
-kaboom()
+kaboom({
+	background: [ 116, 185, 255 ],
+})
 
 // load assets
-loadSprite("ferdi", "https://raw.githubusercontent.com/replit/kaboom/master/assets/sprites/bean.png")
-loadSprite('felix', 'https://raw.githubusercontent.com/replit/kaboom/master/assets/sprites/coin.png')
+loadSprite("ferdi", "https://raw.githubusercontent.com/Barchid/les-aventures-de-casque-dor/master/sprites/ferdi.png")
+loadSprite('felix', 'https://raw.githubusercontent.com/Barchid/les-aventures-de-casque-dor/master/sprites/felix.jpg')
+// loadSprite('felix', 'https://raw.githubusercontent.com/Barchid/les-aventures-de-casque-dor/master/sprites/rami.png')
 
-scene('intro', () => {
-    function addDialog() {
-		const h = 160
-		const pad = 16
-		const bg = add([
-			pos(0, height() - h),
-			rect(width(), h),
-			color(0, 0, 0),
-			z(100),
-		])
-		const txt = add([
-			text("", {
-				width: width(),
-			}),
-			pos(0 + pad, height() - h + pad),
-			z(100),
-		])
-		bg.hidden = true
-		txt.hidden = true
-		return {
-			say(t) {
-				txt.text = t
-				bg.hidden = false
-				txt.hidden = false
-			},
-			dismiss() {
-				if (!this.active()) {
-					return
-				}
-				txt.text = ""
-				bg.hidden = true
-				txt.hidden = true
-			},
-			active() {
-				return !bg.hidden
-			},
-			destroy() {
-				bg.destroy()
-				txt.destroy()
-			},
-		}
-	}
-
-    const dialog = addDialog()
-
-    // define gravity
-	gravity(2400)
-
-	// add a game object to screen
-	const player = add([
-		// list of components
-		sprite("ferdi"),
-		pos(80, 40),
-		area(),
-		body(),
-	])
-
-	// floor
+scene("intro0", () => {
 	add([
-		rect(width(), FLOOR_HEIGHT),
-		outline(4),
-		pos(0, height()),
-		origin("botleft"),
-		area(),
-		solid(),
-		color(127, 200, 255),
+		text("Les Aventures de Casque d'Or"),
+		pos(width() / 2, height() / 2),
+		origin("center"),
 	])
 
-	function jump() {
-		if (player.isGrounded()) {
-			player.jump(JUMP_FORCE)
-            canSecondJump = true
-		}
-        else if (canSecondJump){
-            player.jump(JUMP_FORCE * 0.75)
-            canSecondJump = false
-        }
-	}
+    add([
+		text("(Espace pour continuer, fdp)"),
+		pos(width() / 2, height() / 1.2),
+		origin("center"),
+	])
 
-    player.onGround(() => {
-        canSecondJump = false
-    })
+    onKeyPress("space", () => go("intro1"))
+})
 
-	// jump when user press space
-	onKeyPress("space", jump)
-	onClick(jump)
-    onKeyDown("left", () => {
-		player.move(-MOVE_SPEED, 0)
-	})
+scene("intro1", () => {
+	add([
+		text("Chapitre 1: de retour de l'ecole"),
+		pos(width() / 2, height() / 2),
+		origin("center"),
+	])
 
-	onKeyDown("right", () => {
-		player.move(MOVE_SPEED, 0)
-	})
+    onKeyPress("space", () => go("intro2"))
+})
+
+scene("intro2", () => {
+	add([
+		text("En rentrant chez lui, MC Casque d'Or\n(FDP, en abrege) ne se doutait pas\nqu'il devra faire face aux membres du\nSouverain Gang\n\n(en vrai il y en a un seul parce que\nj'avais la flemme de tous vous coder)."),
+		pos(0 + width() / 20, 0 + height()/3),
+		origin("left"),
+	])
+
+    onKeyPress("space", () => go("intro3"))
+})
+
+scene("intro3", () => {
+	add([
+		text("Dans son periple, FDP rencontra d'abord\nVliegen qui echangeait des snaps\n\n-'Que fais-tu, mon ami ?', dit FDP\n\nVliegen lui expliqua qu'il etait en\npleine etude de marche pour trouver\nla pute la moins chere de Bx.\n\nAyant finalement trouve la legendaire\npute Lidl, Vliegen a maintenant besoin\nde 50 balles."),
+		pos(0 + width() / 20, 0 + height()/2),
+		origin("left"),
+	])
+
+    onKeyPress("space", () => go("intro4"))
+})
+
+scene("intro4", () => {
+	add([
+		text("Vous avez vraiment envie d'aider !\n\nRamassez un max de billets de 10 euros\npour aider Vliegen a completer sa\nlevee de fonds."),
+		pos(0 + width() / 20, 0 + height()/2),
+		origin("left"),
+	])
+
+    onKeyPress("space", () => go("intro5"))
+})
+
+scene("intro5", () => {
+	add([
+		text("Appuyez sur Espace pour sauter\n(double saut possible)\n\n GO ?! Espace et commencer"),
+		pos(0 + width() / 20, 0 + height()/2),
+		origin("left"),
+	])
+
+    onKeyPress("space", () => go("game"))
 })
 
 scene("game", () => {
@@ -132,7 +106,7 @@ scene("game", () => {
 		origin("botleft"),
 		area(),
 		solid(),
-		color(127, 200, 255),
+		color(107, 62, 46),
 	])
 
 	function jump() {
@@ -163,7 +137,7 @@ scene("game", () => {
 			outline(4),
 			pos(width(), height() - FLOOR_HEIGHT),
 			origin("botleft"),
-			color(255, 180, 255),
+			color(46, 204, 113),
 			move(LEFT, SPEED),
 			cleanup(),
 			"tree",
@@ -192,9 +166,36 @@ scene("game", () => {
 		pos(24, 24),
 	])
 
+    function spawnCoins() {
+
+		// add coin obj
+		add([
+			sprite('felix'),
+			area(),
+			outline(4),
+            scale(2),
+			pos(width(), height() - FLOOR_HEIGHT - 150 - randi(0, 50)),
+			origin("botleft"),
+			move(LEFT, SPEED),
+			cleanup(),
+			"felix",
+		])
+
+		// wait a random amount of time to spawn next tree
+		wait(rand(0.5, 4), spawnCoins)
+
+	}
+
+    spawnCoins()
+
+    player.onCollide("felix", (c) => {
+		destroy(c)
+        score += 1
+        scoreLabel.text = score
+	})
+
 	// increment score every frame
 	onUpdate(() => {
-		score++
 		scoreLabel.text = score
 	})
 
@@ -204,16 +205,15 @@ scene("lose", (score) => {
 
 	add([
 		sprite("ferdi"),
-		pos(width() / 2, height() / 2 - 80),
+		pos(width() / 2, height() / 2 - 160),
 		scale(2),
 		origin("center"),
 	])
 
 	// display score
 	add([
-		text(score),
+		text(score + ' billets recuperes soit\n' + score/5 + ' putes Lidl baisees.\n\nEspace pour recommencer.'),
 		pos(width() / 2, height() / 2 + 80),
-		scale(2),
 		origin("center"),
 	])
 
@@ -223,4 +223,4 @@ scene("lose", (score) => {
 
 })
 
-go("intro")
+go("intro0")
